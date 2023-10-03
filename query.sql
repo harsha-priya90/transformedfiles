8a2bffde46c1f3e25192d00e96d9fcba9989a64e
harsha-priya90 <145891147+harsha-priya90@users.noreply.github.com>
2023-10-03 09:57:11 +0530

    query

---
+++ query.sql
@@ -0,0 +1,9 @@
+create table dailyhospital(country char (100),
+countrycode2digit int,
+countrycode3digit int,
+population int,
+reporteddate varchar(50), 
+Dailyhospital int,
+DailyICUoccupancy int,
+sourcename char(90))
+select * from dailyhospital
