2dbec3a81ab8a421e6f07e69345903d72e56a334
harsha-priya90 <145891147+harsha-priya90@users.noreply.github.com>
2023-10-03 10:19:50 +0530

    weekly

---
+++ query weekly.sql
@@ -0,0 +1,12 @@
+create table weeklyhospital(country char(90),
+countrycode2_digit int,
+countrycode3digit int,
+reported_date varchar(90),
+populationdata int,
+reportedyearweek varchar(90),
+reportedweekstartdate varchar(90),
+reportedweekenddate varchar(90),
+newWeeklynewhospitaladmissionsper100k varchar(90),
+newWeeklynewICUadmissionsper100k varchar(90),
+sourcename varchar(90));
+select * from  weeklyhospital;
